c4d9e39259b20e432efdb80c13d1414fd117876d
agwebberley <agwebberley@pm.me>
2025-03-15 17:10:06 -0700

    Basic Clap Init

---
+++ src/main.rs
@@ -0,0 +1,21 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    // The Short and long options for the name argument
+    // are set to -n and --name respectively
+    #[arg(short, long)]
+    name: String,
+    #[arg(short, long, default_value_t = 1)]
+    count: u8,
+}
+
+
+fn main() {
+    let args = Args::parse();
+
+    for _ in 0..args.count {
+        println!("Hello {}!", args.name);
+    }
+}
